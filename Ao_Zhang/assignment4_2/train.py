535852b6c5a3467270174a0cf22db18b6b435964
Ao Zhang <zhangaocanada@gmail.com>
2019-11-07 23:34:45 -0500

    update

--- Ao_Zhang/assignment4_2/train.py
+++ Ao_Zhang/assignment4_2/train.py
@@ -3,6 +3,10 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 import glob
 import imageio
 import matplotlib.pyplot as plt
