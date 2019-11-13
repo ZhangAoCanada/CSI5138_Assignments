8a0b094a8bfb97dce8dc95577df5b133bfaee6b5
ZhangAoCanada <zhangaocanada@gmail.com>
2019-11-13 12:45:27 -0500

    update

--- Ao_Zhang/assignment4_2/read.py
+++ Ao_Zhang/assignment4_2/read.py
@@ -1,5 +1,19 @@
+"""
+-----------------------------------------------------------------
+CSI 5138: Assignment 4
+Student:            Ao   Zhang
+Student Number:     0300039680
+
+-----------------------------------------------------------------
+Generated images reading code for the assignment 4.
+
+The code is for reading the generated samples saved during the 
+training process.
+-----------------------------------------------------------------
+"""
 import numpy as np
 import matplotlib
+# for remote plot through X11
 matplotlib.use("tkagg")
 import matplotlib.pyplot as plt
 from glob import glob
@@ -18,6 +32,7 @@ def ReadAllSamples(model_name, dataset_name, num_hidden, latent_size, hidden_lay
     Function:
         Read all files under the current directory
     """
+    # get the right files
     dir_n = "samples/" + model_name + "_" + dataset_name + "_" + str(num_hidden) + "_" + \
                 str(latent_size) + "_" + str(hidden_layer_size) + "/"
     all_files = glob(dir_n + "*.npy")
@@ -25,7 +40,8 @@ def ReadAllSamples(model_name, dataset_name, num_hidden, latent_size, hidden_lay
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    for file_id in range(1000, num_files):
+    for file_id in range(450, num_files):
+        # get image sequence number
         file_n = "samples/" + model_name + "_" + dataset_name + "_" + str(num_hidden) + \
                     "_" + str(latent_size) + "_" + str(hidden_layer_size) + "/" + str(file_id) + ".npy"
         current_samples = ReadSamples(file_n)
@@ -35,7 +51,7 @@ def ReadAllSamples(model_name, dataset_name, num_hidden, latent_size, hidden_lay
             current_samples = current_samples.reshape((20, 30, 32, 32, 3))
 
         all_imgs = []
-        for i in range(5):
+        for i in range(10):
             row_imgs = []
             for j in range(10):
                 row_imgs.append(current_samples[i, j])
@@ -49,9 +65,7 @@ def ReadAllSamples(model_name, dataset_name, num_hidden, latent_size, hidden_lay
         ax1.axis("off")
         ax1.set_title(str(file_id))
         fig.canvas.draw()
-        plt.pause(0.5)
-
-
+        plt.pause(1)
 
 if __name__ == "__main__":
     """
@@ -67,24 +81,7 @@ if __name__ == "__main__":
     dataset_name = "CIFAR"
 
     num_hidden = 0
-    latent_size = 50
+    latent_size = 100
     hidden_layer_size = 256
 
     ReadAllSamples(model_name, dataset_name, num_hidden, latent_size, hidden_layer_size)
-
-
-    # model_names = ["VAE", "GAN", "WGAN"]
-    # dataset_names = ["MNIST"]
-
-    # num_hiddens = [4, 5]
-    # latent_sizes = [10, 50, 100, 200]
-    # hidden_layer_sizes = [128, 256, 512, 1024]
-    # hidden_layer_size = 256
-
-    # for dataset_name in dataset_names:
-    #     for num_hidden in num_hiddens:
-    #         for latent_size in latent_sizes:
-    #             for model_name in model_names:
-    #                 ReadAllSamples(model_name, dataset_name, num_hidden, \
-    #                                             latent_size, hidden_layer_size)
-        
--- Ao_Zhang/assignment4_2/train.py
+++ Ao_Zhang/assignment4_2/train.py
@@ -1,17 +1,29 @@
-#
+"""
+-----------------------------------------------------------------
+CSI 5138: Assignment 4
+Student:            Ao   Zhang
+Student Number:     0300039680
+
+-----------------------------------------------------------------
+Training code for the assignment 4.
+
+The code is for training the models. The training process is built
+as the requirements of our assignment description. Note that the 
+TensorFlow dependency of this code is:            TensorFlow 2.0
+-----------------------------------------------------------------
+"""
 ##### set specific gpu #####
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import tensorflow as tf
+##### 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 import glob
 import imageio
-# import matplotlib
-# matplotlib.use("tkagg")
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
@@ -20,7 +32,7 @@ import time
 import pickle
 from tqdm import tqdm
 from mlxtend.data import loadlocal_mnist
-
+# import models
 from vae import vae
 from gan import gan
 from wgan import wgan
@@ -74,6 +86,10 @@ def GetMnistData(data_path):
 
 ############################## CIFAR LOADING ############################
 def ReadCifarLabels(cfar_dir):
+    """
+    Function:
+        Read all label names from given cifar files
+    """
     file_name = cfar_dir + "batches.meta"
     with open(file_name, 'rb') as fo:
         cfar_dict = pickle.load(fo, encoding='bytes')
@@ -84,6 +100,10 @@ def ReadCifarLabels(cfar_dir):
     return label_names
 
 def ReadCifarData(file_name):
+    """
+    Function:
+        Read all training data from given cifar files
+    """
     with open(file_name, 'rb') as fo:
         cfar_dict = pickle.load(fo, encoding='bytes')
     data = cfar_dict[b'data']
@@ -93,19 +113,27 @@ def ReadCifarData(file_name):
     return data, labels, data.shape[1]
 
 def LoadAllCifarData(cfar_dir):
+    """
+    Function:
+        Read all training and testing data from the given cifar directory.
+    """
+    # all files in the cifar directory
     batch_file_1 = cfar_dir + "data_batch_1"
     batch_file_2 = cfar_dir + "data_batch_2"
     batch_file_3 = cfar_dir + "data_batch_3"
     batch_file_4 = cfar_dir + "data_batch_4"
     batch_file_5 = cfar_dir + "data_batch_5"
     batch_file_test = cfar_dir + "test_batch"
+    # concatenate all file names into a list
     all_batch_train = [batch_file_1, batch_file_2, batch_file_3, batch_file_4, batch_file_5]
     X_train = []
     Y_train = []
+    # read each file and transfer the data into numpy array
     for each_file in all_batch_train:
         data, labels, input_size = ReadCifarData(each_file)
         X_train.append(data)
         Y_train.append(labels)
+    # concatenate all training data and testing data (normalize it into [0, 1])
     X_train = np.concatenate(X_train, axis = 0).astype(np.float32) / 255.
     Y_train = np.concatenate(Y_train, axis = 0).astype(np.float32)
     X_test, Y_test, input_size = ReadCifarData(batch_file_test)
@@ -115,6 +143,12 @@ def LoadAllCifarData(cfar_dir):
     return X_train, Y_train, X_test, Y_test, input_size
 
 def TransferToImage(data):
+    """
+    Function:
+        Since the format of CIFAR data is arranged into R, G, B
+    separately. Here, we transfer the format into the standard format
+    in order to visualize the data.
+    """
     r_channel = data[:, :1024].reshape((-1, 32, 32, 1))
     g_channel = data[:, 1024:2048].reshape((-1, 32, 32, 1))
     b_channel = data[:, 2048:].reshape((-1, 32, 32, 1))
@@ -122,6 +156,10 @@ def TransferToImage(data):
     return imgs
 
 def FormSamples(dataset_name, current_samples):
+    """
+    Function:
+        Reshape the output samples for visualization
+    """
     if dataset_name == "MNIST":
         current_samples = current_samples.reshape((10, 20, 28, 28))
     else:
@@ -137,8 +175,11 @@ def FormSamples(dataset_name, current_samples):
     all_imgs = np.concatenate(all_imgs, axis = 0)
     return all_imgs
 
+############################## MAIN FUNCTION ############################
 def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_save=True):
+    # reset graph
     tf.keras.backend.clear_session()
+    # get input according to the dataset name
     if dataset_name == "MNIST":
         Mnist_local_path = "mnist/"
         X_train, Y_train, X_test, Y_test, input_size, output_size = GetMnistData(Mnist_local_path)
@@ -157,12 +198,14 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
     else:
         input_size = (28, 28, 1)
 
+    # training hyper-parameters settings
     batch_size = 256
-    epochs = 1500
+    epochs = 1000
     hm_batches_train = len(X_train) // batch_size
     hidden_layer_size = 256 # feel free to tune
     sample_size = 600
 
+    # get model according to different model names
     if model_name == "VAE":
         model = vae(input_size, num_hidden, hidden_layer_size, latent_size, batch_size, dataset_name)
     elif model_name == "GAN":
@@ -172,6 +215,7 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
     else:
         raise ValueError("Please input the right model name.")
 
+    # set check points
     checkpoint_dir = './checkpoints/' + model_name + "_" + dataset_name + "_" + \
                 str(num_hidden) + "_" + str(latent_size) + "_" + str(hidden_layer_size)
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
@@ -185,7 +229,7 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
                                         generator=model.gen,
                                         discriminator=model.disc)
     
-
+    # set tensorboard
     log_dir = "logs/" + model_name + "_" + dataset_name + "_" + \
             str(num_hidden) + "_" + str(latent_size) + "_" + str(hidden_layer_size)
     summary_writer = tf.summary.create_file_writer(log_dir)
@@ -197,28 +241,24 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
         ax = fig.add_subplot(111)
     
     counter = 0
-    # np.random.shuffle(X_train)
-
     for epoch_id in tqdm(range(epochs)):
         for each_batch_train in range(hm_batches_train):
             # X_train_batch = X_train[each_batch_train*batch_size: (each_batch_train+1)*batch_size]
             batch_idx = np.random.randint(len(X_train), size=batch_size)
             X_train_batch = X_train[batch_idx]
 
+            # for GAN and WGAN, we use tanh as activation function. Therefore, the input should be
+            # transferred into the same format.
             if model_name == "GAN" or model_name == "WGAN":
                 X_train_batch = (X_train_batch - 0.5) / 0.5
-
+            
+            # start training
             if model_name == "VAE":
                 v_loss = model.Training(X_train_batch)
             else:
                 g_loss, d_loss = model.Training(X_train_batch)
-            # elif model_name == "GAN":
-            #     g_loss, d_loss = model.Training(X_train_batch)
-            # else:
-            #     for _ in range(3):
-            #         d_loss = model.Training_D(X_train_batch)
-            #     g_loss = model.Training_G(X_train_batch)
-            
+
+            # write into tensorboard
             with summary_writer.as_default():
                 if model_name == "VAE":
                     tf.summary.scalar(model_name + '_loss', v_loss.numpy(), step=epoch_id*hm_batches_train + each_batch_train)
@@ -226,8 +266,10 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
                     tf.summary.scalar(model_name + '_G_loss', g_loss.numpy(), step=epoch_id*hm_batches_train + each_batch_train)
                     tf.summary.scalar(model_name + '_D_loss', d_loss.numpy(), step=epoch_id*hm_batches_train + each_batch_train)
 
+            # sampling the results for a specific frequency
             if (epoch_id*batch_size + each_batch_train) % 200 == 0:
                 seed = tf.random.normal([sample_size, latent_size])
+                # sampling
                 if model_name == "VAE":
                     samples = model.Decoding(seed, apply_sigmoid=True)
                 else:
@@ -236,6 +278,7 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
                 if model_name == "GAN" or model_name == "WGAN":
                     samples = 0.5 * samples + 0.5
 
+                # save the results as numpy arrays
                 if if_save:
                     dir_n = "samples/" + model_name + "_" + dataset_name + "_" + str(num_hidden) + "_" + str(latent_size) + "_" + str(hidden_layer_size)
                     if not os.path.exists(dir_n):
@@ -245,6 +288,7 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
                     np.save(file_n, samples)
                     counter += 1
 
+                # for visualization during the training (if debug is needed)
                 if if_plot:
                     disp_imgs = FormSamples(dataset_name, samples)
                     plt.cla()
@@ -253,7 +297,8 @@ def train(model_name, dataset_name, num_hidden, latent_size, if_plot=False, if_s
                     ax.imshow(disp_imgs)
                     fig.canvas.draw()
                     plt.pause(0.01)
-            
+
+            # save the model as checkpoint.
             if (epoch_id*batch_size + each_batch_train) % 5000 == 0:
                 checkpoint.save(file_prefix = checkpoint_prefix)
             
@@ -270,9 +315,9 @@ if __name__ == "__main__":
         "CIFAR"
     """
     model_names = ["GAN", "WGAN", "VAE"]
-    lantent_sizes = [100, 50, 10, 20]
+    lantent_sizes = [100, 50]
     num_hiddens = [0, 1, 2, 3]
-    
+
     for latent_size in lantent_sizes:
         for model_name in model_names:
             train(model_name, "CIFAR", 0, latent_size)
@@ -280,5 +325,3 @@ if __name__ == "__main__":
     for num_hidden in num_hiddens:
         for model_name in model_names:
             train(model_name, "CIFAR", num_hidden, 100)
-
-    # train("WGAN", "CIFAR", 0, 50)
